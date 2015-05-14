ad5017e0f121021f9961f5c25927e9fb26df6fc8
Ryan Leahy <rleahy@webassign.net>
2015-05-13 22:35:25 -0400

    Initial test setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,8 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-wiredep');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-istanbul-coverage');
 
     grunt.initConfig({
         wiredep: {
@@ -23,6 +25,24 @@ module.exports = function (grunt) {
             options: {
                 jshintrc: '.jshintrc'
             }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        coverage: {
+            default: {
+                options: {
+                    thresholds: {
+                        'statements': 5
+                    },
+                    dir: 'coverage',
+                    root: './'
+                }
+            }
         }
     });
+
+    grunt.registerTask('ci', ['karma', 'coverage']);
 };
---
+++ public/modules/player/player.controllers.spec.js
@@ -0,0 +1,24 @@
+"use strict";
+
+describe("PlayerCtrl", function() {
+    var vm = {};
+
+    beforeEach(function() {
+        bard.appModule('app', function($provide) {
+            $provide.value('configService', {
+                youtubeKey: 'fakeKey'
+            })
+        });
+
+        bard.inject(this, '$controller');
+
+        vm = $controller('PlayerCtrl');
+    });
+
+    describe('search', function() {
+        it('should initialize a playlist object', function() {
+            expect(vm.playlist.length).toEqual(2);
+            expect(vm.playlist[0].title).toMatch(/YouTube/);
+        });
+    });
+})
